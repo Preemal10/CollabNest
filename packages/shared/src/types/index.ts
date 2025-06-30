417c0a8d8087accc22d188032abc33554b8f9806
Preemal10 <preemal2610@gmail.com>
2025-06-30 16:28:39 +0100

    feat(shared): add type exports and constants

---
+++ packages/shared/src/types/index.ts
@@ -0,0 +1,32 @@
+// User types
+export * from './user.js';
+
+// Organization types
+export * from './organization.js';
+
+// Project types
+export * from './project.js';
+
+// Board types
+export * from './board.js';
+
+// Task types
+export * from './task.js';
+
+// Comment types
+export * from './comment.js';
+
+// Attachment types
+export * from './attachment.js';
+
+// Activity types
+export * from './activity.js';
+
+// Notification types
+export * from './notification.js';
+
+// API types
+export * from './api.js';
+
+// WebSocket types
+export * from './websocket.js';
